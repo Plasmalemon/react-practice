c4222fecb8b145b72f89a94af8e7d85a02ff5f18
plasmalemon <781888977@qq.com>
2020-02-09 20:41:28 +0800

    feat: createPortal hooks实现方式

--- src/pages/level3/components/Dialog.js
+++ src/pages/level3/components/Dialog.js
@@ -1,39 +1,62 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect } from 'react'
 import { createPortal } from 'react-dom';
 
 // 所有的逻辑写在组件里边, 组件暴露api给外边调用就可以了
 // 传送门，react v16之后出现的portal可以实现内容传送功能。
 
 
-class Dialog extends Component {
-    constructor(props) {
-        super(props)
+// 实现一class组件
+// class Dialog extends Component {
+//     constructor(props) {
+//         super(props)
 
-        this.state = {
+//         this.state = {
 
-        }
-        const doc = window.document
-        this.node = doc.createElement("div")
-        doc.body.appendChild(this.node)
-    }
-    componentWillUnmount() {
-        window.document.body.removeChild(this.node)
-    }
+//         }
+//         const doc = window.document
+//         this.node = doc.createElement("div")
+//         doc.body.appendChild(this.node)
+//     }
+//     componentWillUnmount() {
+//         window.document.body.removeChild(this.node)
+//     }
 
-    // ReactDOM.render(vdom, container)
-    // createPortal(vdom, container)
+//     // ReactDOM.render(vdom, container)
+//     // createPortal(vdom, container)
 
-    render() {
-        return createPortal(
-            <div className="dialog">
-                <h4>Dialog</h4>
-            </div>,
-            this.node
-        )
-    }
+//     render() {
+//         return createPortal(
+//             <div className="dialog">
+//                 <h4>Dialog</h4>
+//             </div>,
+//             this.node
+//         )
+//     }
+// }
+
+// export default Dialog
+
+// 方法二 函数组件
+export default function Dialog() {
+    const doc = window.document;
+    const node = doc.createElement("div");
+    doc.body.appendChild(node);
+    useEffect(() => {
+        return () => {
+            window.document.body.removeChild(node);
+        };
+    }, []);
+    return createPortal(
+        <div className="dialog">
+            <h1>dialog</h1>
+        </div>,
+        node,
+        // window.document.body
+    );
 }
 
-export default Dialog
+// 16.0以前的 没有暴露 api  unmountComponentAtNode  unstable_renderSubtreeIntoContainer
+
 // export class Dialog2 extends React.Component {
 //     render() {
 //         return null;
